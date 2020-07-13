020b3844e009f8e50676c2264c98d9935751ec7c
alexgil37 <40404788+alexgil37@users.noreply.github.com>
2020-07-13 12:54:17 -0700

    Different format files added for testing

--- ModifyEffCell.py
+++ ModifyEffCell.py
@@ -17,12 +17,14 @@ def getListOfFiles(dirName):
 def find_instrument_model_cell(currentSheet):
     for row in range(1, 50):
         for column in "ABCDEFGHIJKLMNOPQRSTUV":  # Here you can add or reduce the columns
-            cell_name = "{}{}".format(column, row)
-            if currentSheet[cell_name].value == instrumentModel:
+            modelCell = "{}{}".format(column, row)
+            if currentSheet[modelCell].value == instrumentModel:
                 #print("{1} cell is located on {0}" .format(cell_name, currentSheet[cell_name].value))
                 #print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
-                print("the row is {0} and the column {1}" .format(cell_name[1], cell_name[0]))
-                return cell_name
+                print("the row is {0} and the column {1}" .format(modelCell[1], modelCell[0]))
+                print(currentSheet[modelCell].value)
+                print(modelCell)
+                return modelCell
 
 def find_instrument_sn_cell(instModelCell):
     snRow = str(int(instModelCell[1]) + 1)
@@ -39,7 +41,7 @@ def find_instrument_sn_cell(instModelCell):
 
 def find_instrument_efficiency(instModelCell):
     effRow = str(int(instModelCell[1]) + 3)
-    effCol = chr(ord(instModelCell[0]) + 2)
+    effCol = chr(ord(instModelCell[0]) + 3)
     effCell = currentSheet[effCol + effRow]
 
     print(effCell.value)
@@ -75,6 +77,7 @@ for file in files:
         print("Current sheet name is {}" .format(x))
         currentSheet = theFile[x]
         instModelCell = find_instrument_model_cell(currentSheet)
+        print(instModelCell)
         if instModelCell is None:
             continue
         instSNcell = find_instrument_sn_cell(instModelCell)
@@ -93,25 +96,3 @@ for file in files:
     theFile.save(file)
 
 print("The files with no s/n are {}".format(filesWithNoSN))
-
-
-
-
-"""
-Purpose: This function is used to create a list of all files within a designated folder and then return the list. The 
-            origin folder must be within the same folder as the program.
-Parameter(s): The parameter is a folder name/folder path from the current directory of the program.
-Return: Returns a list of all file names, including file path, within the folder as well as all subdirectories.
-
-def getListOfFiles(dirName):
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    for file in listOfFile:
-        fullPath = os.path.join(dirName, file)
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-    print(allFiles)
-    return allFiles
-"""
