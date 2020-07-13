22aef8b32abe6c9882e5ca545981ecf4b8fc206d
alexgil37 <40404788+alexgil37@users.noreply.github.com>
2020-07-13 14:08:00 -0700

    Edge cases tested and code works for different formast

--- ModifyEffCell.py
+++ ModifyEffCell.py
@@ -2,33 +2,40 @@ import openpyxl
 import json
 import os
 
+
 def getListOfFiles(dirName):
     listOfFile = os.listdir(dirName)
     allFiles = list()
+
     for file in listOfFile:
         fullPath = os.path.join(dirName, file)
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFiles(fullPath)
         else:
             allFiles.append(fullPath)
+
     print(allFiles)
+
     return allFiles
+
 
 def find_instrument_model_cell(currentSheet):
     for row in range(1, 50):
         for column in "ABCDEFGHIJKLMNOPQRSTUV":  # Here you can add or reduce the columns
             modelCell = "{}{}".format(column, row)
             if currentSheet[modelCell].value == instrumentModel:
-                #print("{1} cell is located on {0}" .format(cell_name, currentSheet[cell_name].value))
-                #print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
-                print("the row is {0} and the column {1}" .format(modelCell[1], modelCell[0]))
+                print("the row is {0} and the column {1}" .format(row, column))
                 print(currentSheet[modelCell].value)
                 print(modelCell)
-                return modelCell
 
-def find_instrument_sn_cell(instModelCell):
-    snRow = str(int(instModelCell[1]) + 1)
-    snCol = instModelCell[0]
+                return [row, column, modelCell]
+
+    return [0, 0, None]
+
+
+def find_instrument_sn_cell(instModelRow, instModelColumn):
+    snRow = str(int(instModelRow) + 1)
+    snCol = instModelColumn
     snCell = currentSheet[snCol + snRow]
 
     print('xxxxxxxxxxx')
@@ -39,10 +46,14 @@ def find_instrument_sn_cell(instModelCell):
     return snCell
 
 
-def find_instrument_efficiency(instModelCell):
-    effRow = str(int(instModelCell[1]) + 3)
-    effCol = chr(ord(instModelCell[0]) + 3)
+def find_instrument_efficiency(instModelRow, instModelColumn):
+    effRow = str(int(instModelRow) + 3)
+    effCol = chr(ord(instModelColumn) + 2)
     effCell = currentSheet[effCol + effRow]
+
+    if type(effCell).__name__ == 'MergedCell':
+        effCol = chr(ord(instModelColumn) + 3)
+        effCell = currentSheet[effCol + effRow]
 
     print(effCell.value)
     print(effRow)
@@ -50,17 +61,20 @@ def find_instrument_efficiency(instModelCell):
 
     return effCell
 
+
 def modify_efficiency(instSNcell, instEfficiencyCell):
     for inst in instrumentsData:
         if inst['sn'] == instSNcell.value:
             instEfficiencyCell.value = inst['betaEfficiency']
+
             return inst['sn']
 
 
 instrumentModel = '2360/43-93'
 #instrumentId = '227413/PR295918'
 
-filesWithNoSN = list()
+filesWithNoMatchingSN = list()
+sheetsOfFilesWithNoMatchingSN = list()
 
 files = getListOfFiles('surveys')
 
@@ -76,23 +90,23 @@ for file in files:
     for x in allSheetNames:
         print("Current sheet name is {}" .format(x))
         currentSheet = theFile[x]
-        instModelCell = find_instrument_model_cell(currentSheet)
-        print(instModelCell)
+        instModelRow = find_instrument_model_cell(currentSheet)[0]
+        instModelColumn = find_instrument_model_cell(currentSheet)[1]
+        instModelCell = find_instrument_model_cell(currentSheet)[2]
+
+        print("The cell is {}, the row is {} and the column is {} ".format(instModelCell, instModelRow, instModelColumn))
+
         if instModelCell is None:
             continue
-        instSNcell = find_instrument_sn_cell(instModelCell)
-        instEfficiencyCell = find_instrument_efficiency(instModelCell)
+        instSNcell = find_instrument_sn_cell(instModelRow, instModelColumn)
+        instEfficiencyCell = find_instrument_efficiency(instModelRow, instModelColumn)
         serialNumber = modify_efficiency(instSNcell, instEfficiencyCell)
 
         if serialNumber is None:
-            filesWithNoSN.append(file)
-
-        #print(currentSheet['L8'].value)
-
-        # if currentSheet['L8'].value == instrumentId:
-        #     currentSheet['N10'].value = 0.152
+            filesWithNoMatchingSN.append(file)
+            sheetsOfFilesWithNoMatchingSN.append(currentSheet)
 
     theFile.close()
     theFile.save(file)
 
-print("The files with no s/n are {}".format(filesWithNoSN))
+print("The files with no s/n are {}, the sheet is {}".format(filesWithNoMatchingSN, sheetsOfFilesWithNoMatchingSN))
